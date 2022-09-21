e087fe16976d9611c4e95add09316c67ec697cd1
Сергей <sergemartin@yandex.ru>
2022-09-21 14:56:48 +0300

    Задача №009

---
+++ task009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 009
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+for (int i = 0; i < n + 1; i++)  
+{
+    if (i % 2 == 0)
+    if (i > 0) Console.WriteLine(i);
+}
