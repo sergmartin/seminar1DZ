88eb6d4fc945d64c73aa0de0718f1f7eebda6b16
Сергей <sergemartin@yandex.ru>
2022-09-21 13:34:43 +0300

    Задача №2

---
+++ task002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача №2
+Console.Write("Напишите первое число: ");
+int n1 = Convert.ToInt16(Console.ReadLine());
+Console.Write("Напишите виорое число: ");
+int n2 = Convert.ToInt16(Console.ReadLine());
+
+if (n1 > n2) Console.Write("первое число больше!");
+else Console.Write("Второе число больше!");
