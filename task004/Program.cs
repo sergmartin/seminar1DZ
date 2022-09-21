47b5fd56bfadbfef3d84a5e4071d4f1f0c91735d
Сергей <sergemartin@yandex.ru>
2022-09-21 14:14:37 +0300

    задача номер 006

---
+++ task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №4
+Console.Write("Напишите первое число: ");
+int n1 = Convert.ToInt16(Console.ReadLine());
+Console.Write("Напишите виорое число: ");
+int n2 = Convert.ToInt16(Console.ReadLine());
+Console.Write("Напишите третие число: ");
+int n3 = Convert.ToInt16(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3) Console.Write("первое число самое большое!");
+if (n2 > n1 && n2 > n3) Console.Write("Второе число самое большое!");
+if (n3 > n1 && n3 > n2) Console.Write("Третье число самое большое!");
---
+++ task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача №006
+
+// Запрашиваем число
+Console.Write("Введите число: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+if (n % 2 == 0) Console.Write("Число является четным");
+else Console.Write("Число является не четным");
