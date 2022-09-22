cbe32a416251b963b94691f85a37a7b782e422e8
Сергей <sergemartin@yandex.ru>
2022-09-22 12:09:41 +0300

    добавил раненство чисел и самое маленькое число в задачу№4

--- task004/Program.cs
+++ task004/Program.cs
@@ -6,7 +6,10 @@ int n2 = Convert.ToInt16(Console.ReadLine());
 Console.Write("Напишите третие число: ");
 int n3 = Convert.ToInt16(Console.ReadLine());
 
-if (n1 > n2 && n1 > n3) Console.Write("первое число самое большое!");
-if (n2 > n1 && n2 > n3) Console.Write("Второе число самое большое!");
-if (n3 > n1 && n3 > n2) Console.Write("Третье число самое большое!");
-if (n3 == n1 && n3 == n2) Console.Write("Числа равны!");
+if (n1 > n2 && n1 > n3) Console.WriteLine("Первое число самое большое!");
+if (n2 > n1 && n2 > n3) Console.WriteLine("Второе число самое большое!");
+if (n3 > n1 && n3 > n2) Console.WriteLine("Третье число самое большое!");
+if (n3 == n1 && n3 == n2) Console.WriteLine("Числа равны!");
+if (n1 < n2 && n1 < n3) Console.WriteLine("Первое число самое маленькое!");
+if (n2 < n1 && n2 < n3) Console.WriteLine("Второе число самое маленькое!");
+if (n3 < n1 && n3 < n2) Console.WriteLine("Третье число самое маленькое!");
