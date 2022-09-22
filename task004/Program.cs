cbc8b5d575033e4d631b0027a549bd51abaf7a29
Сергей <sergemartin@yandex.ru>
2022-09-22 11:53:25 +0300

    поправил задачу №4

--- task004/Program.cs
+++ task004/Program.cs
@@ -9,3 +9,4 @@ int n3 = Convert.ToInt16(Console.ReadLine());
 if (n1 > n2 && n1 > n3) Console.Write("первое число самое большое!");
 if (n2 > n1 && n2 > n3) Console.Write("Второе число самое большое!");
 if (n3 > n1 && n3 > n2) Console.Write("Третье число самое большое!");
+if (n3 == n1 && n3 == n2) Console.Write("Числа равны!");
